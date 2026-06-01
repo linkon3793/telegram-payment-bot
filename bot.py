5b697d3264b73a05bcc6a4ecdb3c1d3bcd2b71f7
linkon3793 <linkonw811@gmail.com>
2026-06-02 03:00:11 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Bot setup started")
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+bot.reply_to(
+message,
+"স্বাগতম VIP1 Membership বটে।"
+)
+
+print("Bot running...")
+bot.infinity_polling()
